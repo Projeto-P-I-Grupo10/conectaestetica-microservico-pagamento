a55ba0fd1a56b3640cc8e5bb398fc2ce4fcbeb0a
MateusSilvaSPTECH <mateus.ssouza@sptech.school>
2026-04-19 23:10:15 -0300

    Feat: Adicionando endpoint que trara o status do pagamento de um curso x do usuario y

--- pagamento/pagamento/pagamento/src/main/java/school/sptech/repository/IPagamentoRepository.java
+++ pagamento/pagamento/pagamento/src/main/java/school/sptech/repository/IPagamentoRepository.java
@@ -4,5 +4,10 @@ package school.sptech.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import school.sptech.model.Pagamento;
 
+import java.util.Optional;
+
 public interface IPagamentoRepository extends JpaRepository<Pagamento,Long> {
+    Optional<Pagamento> findByIdUsuarioAndIdCursoAndStatus(Long idUsuario, Long idCurso, String status);
+    Optional<Pagamento> findByIdCursoAndIdUsuario(Long idCurso, Long idUsuario);
+
 }
